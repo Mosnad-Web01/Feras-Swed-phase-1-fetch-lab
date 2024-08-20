2bb24e71a3ccec0423d511ca3de1df3f585912ad
feras-swed <ferasswed2022@gmail.com>
2024-08-20 13:45:36 +0300

    smart

--- index.js
+++ index.js
@@ -19,3 +19,32 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+let myBookArray = [];
+function FivethBook() {
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach(book => {
+        myBookArray.push(book)
+        
+      })
+      console.log(myBookArray[4].name);
+    });
+}
+FivethBook()
+//The total number of pages of all the books
+
+let totalPagesNumber = 0;
+function totalPages() {
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach(book => {
+        totalPagesNumber += book.numberOfPages
+      })
+      console.log(totalPagesNumber);
+    });
+}
+
+totalPages()
